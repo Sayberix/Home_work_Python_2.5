28e0ea2d2b271ab5712899d5191036ce05e79642
Sayberix <mr.maaks@gmail.com>
2022-12-11 16:33:52 +0700

    начало работы над вторым заданием

--- home_work_2.5.py
+++ home_work_2.5.py
@@ -1,18 +1,6 @@
-# Напишите программу, удаляющую из текста все слова, содержащие "абв".
+# 2. Создайте программу для игры с конфетами человек против человека.
 
-from pathlib import Path
-
-def recive_text_file (filename: str) -> str:
-    path_dir = r'1/data/'
-    file_path = Path(path_dir, filename)
-    with open(file_path,'r', encoding="utf-8") as data_str:
-        data_str = data_str.read().split()
-    return data_str
-
-def write_in_file(resault_str: str, filename: str):
-    path_dir = r'1/resault/'
-    file_path = Path(path_dir, filename)
-    with open(file_path,'w', encoding="utf-8") as str_write:
-        str_write.write(str(resault_str))
-
-write_in_file(' '.join(list(filter(lambda el: not "абв" in el, recive_text_file('data.txt')))),'resault.txt')
+# Условие задачи: На столе лежит 2021 конфета. Играют два игрока делая ход друг после друга. 
+# Первый ход определяется жеребьёвкой. За один ход можно забрать не более чем 28 конфет. 
+# Все конфеты оппонента достаются сделавшему последний ход. Сколько конфет нужно взять первому игроку, 
+# чтобы забрать все конфеты у своего конкурента?
