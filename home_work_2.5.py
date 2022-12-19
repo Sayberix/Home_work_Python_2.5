368a81f5a4606b75fdbd3a26460b9c80b7e4b1fd
Sayberix <mr.maaks@gmail.com>
2022-12-19 23:23:53 +0700

    задание выполнено без интерфейса

--- home_work_2.5.py
+++ home_work_2.5.py
@@ -3,45 +3,40 @@
 from pathlib import Path
 import os
 
+# получение текста из файла
 def recive_text_file (directory: str, filename: str) -> str:
     #path_dir = r'4/data_decrypted/'
     file_path = Path(os.getcwd() + directory, os.getcwd() + directory + filename)
-    with open(file_path, encoding="utf-8") as data_str:
-        data_str = data_str.read()
-    return data_str
+    with open(file_path, 'r', encoding="utf-8") as read_data_str:
+        read_data_str = read_data_str.read()
+    return read_data_str
 
-
-
-def write_in_file(resault_str: str, filename: str):
-    path_dir = r'4/data_encrypted/'
-    file_path = Path(path_dir, filename)
+# запись текста в файл
+def write_in_file(resault_str: str, directory: str, filename: str):
+    #path_dir = r'4/data_encrypted/'
+    file_path = Path(os.getcwd() + directory, os.getcwd() + directory + filename)
     with open(file_path,'w', encoding="utf-8") as str_write:
         str_write.write(str(resault_str))
 
+# шифрование данных
 def data_encrypted(text_string: str) -> list:
     #text_string = 'aaababbcbbb'
     list_elements = []
-
     if len(text_string) >= 2:
-        #count_index = 0
         count = 1
         symbol = text_string[0]
         for i in range(1, len(text_string)):
             if text_string[i] == symbol:
                 count += 1
             else:
-                #list_elements.insert(count_index,[text_string[i - 1],count])
                 list_elements.extend([text_string[i - 1],count])
                 symbol = text_string[i]
-                #count_index += 1
                 count = 1
             if i + 1 == len(text_string):
                 list_elements.extend([text_string[i],count])
-    #print(list_elements)
     return list_elements
 
-#def data_decrypted(text_string: str) -> str:
-
+# преобразование строки со списком в удобочитаемый формат
 def format_str(text_string: str) -> str:
     #text_string = "['a', 3, 'b', 1, 'a', 1, 'b', 2, 'c', 1, 'b', 3]"
     text_resault = ""
@@ -50,18 +45,21 @@ def format_str(text_string: str) -> str:
             text_resault += text_string[i]
     return text_resault
 
-#text_str = format_str(recive_text_file('/4/data_decrypted/','data.txt'))
-#print(text_str)
-#for i in range(len(text_resault)):
-#    print(text_resault[i])
-#text_list = text_string[1 : -1].split()
-#print(text_list)
-
-#for i in range(len(text_string)):
-#    text_string[i].replace("'", "")
-#    if i % 2 == 0:
-#print(text_string[1 : -1].split())
+# расшифровка данных
+def data_decrypted(data: str) -> str:
+    #data = "a3b1a1b2c1b3"
+    data = format_str(data) # преобразуем данные для работы над ними как над списком
+    resault = ""
+    for i in range(0, len(data), 2):
+        for j in range(int(data[i + 1])):
+            resault += data[i]
+    return resault
 
 
 # Сжатие данных и их запись в файл
-write_in_file(data_encrypted(recive_text_file('/4/data_decrypted/','data.txt')),'resault.txt')
+data = recive_text_file('/4/data_decrypted/','data.txt')
+write_in_file(data_encrypted(data),'/4/data_encrypted/','resault.txt')
+
+# Распаковка данных и запись их в файл
+data = recive_text_file('/4/data_encrypted/','data.txt')
+write_in_file(data_decrypted(data),'/4/data_decrypted/','resault.txt')
